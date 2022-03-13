f870bc98df119c75ebbb865bc97f5d06bde57e8e
karan sivarat <bomb0069@gmail.com>
2022-03-13 13:26:07 +0700

    [Removed]: adb -s command

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 mkdir -p screenrecord
 adb devices -l
 
-adb -s test shell screenrecord /sdcard/example.mp4 &
+adb shell screenrecord /sdcard/example.mp4 &
 sleep 5
 flutter test integration_test || true
-adb -s test shell pkill -INT screenrecord &
+adb shell pkill -INT screenrecord &
 sleep 15
 adb pull /sdcard/example.mp4 screenrecord/
