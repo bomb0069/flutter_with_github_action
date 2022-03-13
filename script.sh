b49fb3a04269b8b4ef99d255b9f1d92dbaf399ba
karan sivarat <bomb0069@gmail.com>
2022-03-13 12:41:25 +0700

    [Moved]: test record into screenrecord and ignore from git

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+mkdir -p screenrecord
 
 adb shell screenrecord /sdcard/example.mp4 &
 sleep 5
 flutter test integration_test || true
 adb shell pkill -INT screenrecord &
 sleep 15
-adb pull /sdcard/example.mp4
+adb pull /sdcard/example.mp4 screenrecord/
